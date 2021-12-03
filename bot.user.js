2031d2ad3da5914d001a821caf7c5a4dc8a3e274
Michw8888 <95493106+Michw8888@users.noreply.github.com>
2021-12-03 17:21:12 +0100

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -23,7 +23,7 @@ SOFTWARE.*/
 // ==UserScript==
 // @name        AposBot
 // @namespace   AposBot
-// @include     http://agar.io/*
+// @include     https://agar.io
 // @version     3.651
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
